4d1831f51594a4f0e2dc3ac1c79b96c42747530c
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-09-05 13:42:04 +0530

    added buzz sound (click on TV)

--- src/Components/Tv.jsx
+++ src/Components/Tv.jsx
@@ -1,11 +1,13 @@
 import React, { useLayoutEffect, useRef, useState } from "react";
 import tv from "../assets/cursetv.gif";
 import gsap from "gsap";
+import buzzSound from "../assets/buzz.mp3";
 
 const Tv = () => {
   const tvRef = useRef(null);
   const textRef = useRef(null);
   const [click, setClick] = useState(0);
+  const buzzRef = useRef(null);
 
   const msg = {
     2: ["Stop", "clicking", "the", "TV…", "again", "and", "again"],
@@ -34,6 +36,10 @@ const Tv = () => {
   }, [click]);
 
   function tvClick() {
+    if (buzzRef.current) {
+      buzzRef.current.play();
+    }
+
     gsap.to(tvRef.current, {
       x: gsap.utils.random(-700, 700),
       y: gsap.utils.random(-300, 300),
@@ -52,6 +58,8 @@ const Tv = () => {
         className="border  cursor-pointer absolute top-1/3"
         src={tv}
       ></img>
+
+      <audio src={buzzSound} preload="auto" ref={buzzRef}></audio>
 
       {msg[click] && (
         <p
